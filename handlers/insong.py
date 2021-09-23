ab0851746f85709171c38f0128ffb499ece10a7d
BadnamOp <86050674+BadnamOp@users.noreply.github.com>
2021-09-23 20:56:09 +0530

    Update insong.py

--- handlers/insong.py
+++ handlers/insong.py
@@ -4,11 +4,11 @@ from helpers.filters import command
 from callsmusic.callsmusic import client as veez
 
 
-@Client.on_message(command(["z", f"z@{BOT_USERNAME}"]))
+@Client.on_message(command(["B", f"B@{BOT_USERNAME}"]))
 async def songs(client, message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("❗ **song not found.**\n\n**please give a valid song name.**")
+            await message.reply_text("❗ **song not found Lol...**\n\n**please give a valid song name.**")
             return
         text = message.text.split(None, 1)[1]
         results = await veez.get_inline_bot_results(1872165533, f"music {text}")
@@ -16,4 +16,4 @@ async def songs(client, message):
             message.chat.id, results.query_id, results.results[0].id
         )
     except Exception:
-        await message.reply_text("❗ **song not found.**")
+        await message.reply_text("❗ **song not found Lol...**")
